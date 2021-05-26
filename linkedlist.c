9730725fd10324c9f943bda6ec1f5443b0355b7c
roni.abusayeed <roni.abusayeed@gmail.com>
2021-05-26 11:43:55 +0600

    Add linked list api

---
+++ linkedlist.c
@@ -0,0 +1,6 @@
+#include "linkedlist.h"
+
+/** External functions called by linked list api. */
+extern void delete_map(char** grid, int height);
+extern void write_map(char** grid, FILE* stream);
+
---
+++ linkedlist.h
@@ -0,0 +1,31 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+#include <stdio.h>
+
+/** Defines a node in a singly linekd list data structure. */
+typedef struct node
+{
+    char** grid;
+    struct node* next;
+} node_t;
+
+/** Inserts a copy of the map (represented by 2D array of characters)
+ * pointed to by grid at the end of the linked list pointed to by list. 
+ * @param list address of the pointer to the head of the linked list
+ * where to insert the copy of grid. 
+ * @param grid pointer to the 2D array of characters (representing a map)
+ * which is to be copied into the list. */
+void insert_last(node_t** list, char** grid);
+
+/** Writes the contents of the linked list to a given file.
+ * @param list pointer to the head of the linked list whose content
+ * is to be written to the file.
+ * @param filename filename of the file where the list content is
+ * to be written. */
+void write_list(const node_t* list, const char* filename);
+
+/** Frees heap memory associated with list. 
+ * @param list pointer to the head of the singly linked list. */
+void free_list(node_t* list);
+
+#endif  /* LINKEDLIST_H */
