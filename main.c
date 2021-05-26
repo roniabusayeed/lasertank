aff9c886bb14d85fde442c743a3e61deca001d4c
roni.abusayeed <roni.abusayeed@gmail.com>
2021-05-26 17:13:06 +0600

    Give fire power to enemy

--- main.c
+++ main.c
@@ -74,6 +74,14 @@ void initialize_map(char** grid, int height, int width, FILE* map);
  * @param stream file stream where the map is to be written. */
 void write_map(char** grid, int height, int width, FILE* stream);
 
+/** This function is called when the player is in the line of sight
+ * of the enemy tank. 
+ * @param exit_flag exit flag of the program.
+ * @param grid pointer to the 2D array of character representing map.
+ * @param height number of rows in the grid.
+ * @param width number of columns in the grid. /*/
+void enemy_fire(bool* exit_flag, char** grid, int height, int width);
+
 /** Gets a valid menu choice from the user. */
 char menu();
 
@@ -83,7 +91,7 @@ typedef struct
     int x, y;
 } pos_t;
 
-/* Variable to keep trach of player and enemy tanks. */
+/* Variable to keep track of player and enemy tanks. */
 static pos_t player_pos, enemy_pos;
 
 /* An empty game log. */
@@ -144,9 +152,41 @@ int main(int argc, char** argv)
         /* Menu choice from user. */
         char menu_choice;
 
+        /* If the player is in the line of sight of the enemy tank, the enemy
+        tank fires at the player.*/
+        if (player_pos.x == enemy_pos.x)
+        {
+            /* Player is in the horizontal line of sight of the enemy tank. */
+            char enemy_dir;
+            enemy_dir = get_player_dir(grid[enemy_pos.x][enemy_pos.y]);
+            if ((enemy_dir == 'r' && enemy_pos.y < player_pos.y) ||
+            (enemy_dir == 'l' && player_pos.y < enemy_pos.y))
+            {
+                /* Enemy fires. */
+                enemy_fire(&exit_flag, grid, height, width);
+            }
+        }
+        else if (player_pos.y == enemy_pos.y)
+        {
+            /* Player is in the vertical line of sight of the enemy tank. */
+            char enemy_dir;
+            enemy_dir = get_player_dir(grid[enemy_pos.x][enemy_pos.y]);
+            if ((enemy_dir == 'u' && player_pos.x < enemy_pos.x) ||
+            (enemy_dir == 'd' && enemy_pos.x < player_pos.x))
+            {
+                /* Enemy fires. */
+                enemy_fire(&exit_flag, grid, height, width);
+            }
+        }
+
+        /* Check if exit flag is set by enemy tank. */
+        if (exit_flag)
+        {
+            break;
+        }
+
         write_map(grid, height, width, stdout);
         menu_choice = menu();
-        fprintf(stdout, "You chose: %c\n", menu_choice);
 
         /* Go/face up. */
         if (menu_choice == 'w')
@@ -163,7 +203,8 @@ int main(int argc, char** argv)
                 /* The player cannot go to the same position as that of the mirror. */
                 /* The player cannot go out of the boundary of the map. */
                 if (player_pos.x - 1 >= 0 && 
-                !is_mirror(grid[player_pos.x - 1][player_pos.y]))
+                !is_mirror(grid[player_pos.x - 1][player_pos.y]) &&
+                !is_player(grid[player_pos.x - 1][player_pos.y]))
                 {
                     /* Move the player one step upward. */
                     grid[player_pos.x - 1][player_pos.y] = grid[player_pos.x][player_pos.y];
@@ -190,7 +231,8 @@ int main(int argc, char** argv)
                 /* The player cannot go to the same position as that of the mirror. */
                 /* The player cannot go out of the boundary of the map. */
                 if (player_pos.x + 1 < height &&
-                !is_mirror(grid[player_pos.x + 1][player_pos.y]))
+                !is_mirror(grid[player_pos.x + 1][player_pos.y]) &&
+                !is_player(grid[player_pos.x + 1][player_pos.y]))
                 {
                     /* Move the player one step downward. */
                     grid[player_pos.x + 1][player_pos.y] = grid[player_pos.x][player_pos.y];
@@ -217,7 +259,8 @@ int main(int argc, char** argv)
                 /* The player cannot go to the same position as that of the mirror. */
                 /* The player cannot go out of the boundary of the map. */
                 if (player_pos.y + 1 < width &&
-                !is_mirror(grid[player_pos.x][player_pos.y + 1]))
+                !is_mirror(grid[player_pos.x][player_pos.y + 1]) &&
+                !is_player(grid[player_pos.x][player_pos.y + 1]))
                 {
                     /* Move the player one step rightward. */
                     grid[player_pos.x][player_pos.y + 1] = grid[player_pos.x][player_pos.y];
@@ -244,7 +287,8 @@ int main(int argc, char** argv)
                 /* The player cannot go to the same position as that of the mirror. */
                 /* The player cannot go out of the boundary of the map. */
                 if (player_pos.y - 1 >= 0 &&
-                !is_mirror(grid[player_pos.x][player_pos.y - 1]))
+                !is_mirror(grid[player_pos.x][player_pos.y - 1]) && 
+                !is_player(grid[player_pos.x][player_pos.y - 1]))
                 {
                     /* Move the player one step leftward. */
                     grid[player_pos.x][player_pos.y - 1] = grid[player_pos.x][player_pos.y];
@@ -327,7 +371,7 @@ int main(int argc, char** argv)
                 /* If laser hits the enemy tank, declare win and exit. */
                 else if (laser_pos.x == enemy_pos.x && laser_pos.y == enemy_pos.y)
                 {
-                    fprintf(stdout, "Player wis!\n");
+                    fprintf(stdout, "You win!\n");
                     exit_flag = true;
                     break;
                 }
@@ -612,4 +656,103 @@ void write_map(char** grid, int height, int width, FILE* stream)
         fprintf(stream, "*");
     }
     fprintf(stream, "\n");
+}
+
+void enemy_fire(bool* exit_flag, char** grid, int height, int width)
+{
+    /* Laser vector. */
+    pos_t laser_pos;
+    char laser_dir;
+
+    /* Initial position of the laser. */
+    laser_pos.x = enemy_pos.x;
+    laser_pos.y = enemy_pos.y;
+
+    /* Initial direction of the laser. */
+    laser_dir = get_player_dir(grid[enemy_pos.x][enemy_pos.y]);
+
+    /* Laser loop. */
+    while (true)
+    {
+        /* Advance. */
+        if (laser_dir == 'u')
+        {
+            laser_pos.x--;
+        }
+        else if (laser_dir == 'd')
+        {
+            laser_pos.x++;
+        }
+        else if (laser_dir == 'l')
+        {
+            laser_pos.y--;
+        }
+        else if (laser_dir == 'r')
+        {
+            laser_pos.y++;
+        }
+
+        /* If laser is out of bounds, break. */
+        if (laser_pos.x < 0 || laser_pos.x >= height 
+        || laser_pos.y < 0 || laser_pos.y >= width)
+        {
+            break;
+        }
+
+        /* If laser hits the mirror, change direction. */
+        if (is_mirror(grid[laser_pos.x][laser_pos.y]))
+        {
+            /* Get mirror direction: (forward or backward slash) */
+            char mirror_dir;
+            mirror_dir = get_mirror_dir(grid[laser_pos.x][laser_pos.y]);
+
+            /* Forward mirror. */
+            if (mirror_dir == 'f')
+            {
+                /* Change final laser direction based on its initial direction. */
+                if (laser_dir == 'd') laser_dir = 'l';
+                else if (laser_dir == 'u') laser_dir = 'r';
+                else if (laser_dir == 'r') laser_dir = 'u';
+                else if (laser_dir == 'l') laser_dir = 'd';
+            }
+            /* Backward mirror. */
+            else if (mirror_dir == 'b')
+            {
+                if (laser_dir == 'd') laser_dir = 'r';
+                else if (laser_dir == 'u') laser_dir = 'l';
+                else if (laser_dir == 'r') laser_dir = 'd';
+                else if (laser_dir == 'l') laser_dir = 'u';
+            }
+        }
+
+        /* If laser hits the player tank, declare lose and exit. */
+        else if (laser_pos.x == player_pos.x && laser_pos.y == player_pos.y)
+        {
+            fprintf(stdout, "You lose!\n");
+            *exit_flag = true;
+            break;
+        }
+        else
+        {
+            /* The laser is inside the grid, it didn't hit a mirror, and
+            and it didn't hit a player tank. */
+
+            /* Print laser beam. */
+            if (laser_dir == 'u' || laser_dir == 'd')
+            {
+                grid[laser_pos.x][laser_pos.y] = '|';
+            }
+            else
+            {
+                grid[laser_pos.x][laser_pos.y] = '-';
+            }
+            write_map(grid, height, width, stdout);
+            
+            /* Log game. */
+            insert_last(&game_log, grid, height, width);
+            
+            msleep(500);    /* Sleep for 500 ms. */
+            grid[laser_pos.x][laser_pos.y] = ' ';
+        }
+    }
 }
